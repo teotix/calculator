c4ef031094280e45242e4cb14ce38d1e1b0548ee
teotix <teo_kostic@yahoo.com>
2023-12-25 17:42:32 +0100

    Create basic functions and variables

---
+++ script.js
@@ -0,0 +1,24 @@
+function add(a, b) {
+  return a + b;
+}
+
+function substract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return (a / b).toFixed(2);
+}
+
+let firstNum;
+let operator;
+let secondNum;
+
+console.log(add(5, 12));
+console.log(substract(5, 12));
+console.log(multiply(5, 12));
+console.log(divide(5, 12));
